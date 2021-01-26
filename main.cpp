10e239ad1107d09c84d79e16511e928aa49a4c12
Alankvuong <63077857+Alankvuong@users.noreply.github.com>
2021-01-25 16:11:16 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// Author: Alan Vuong
+// Description: Hello World in C++
+//=================== main =======================
+
+#include <iostream>
+using namespace std;
+
+int hello1Function(string name1);
+int hello2SFunction(string name2);
+
+int main()
+{
+	string name1 = "Alan Vuong";
+	hello1Function(name1);
+	string name2 = "Justin Chiu";
+	hello2Function(name2);
+
+	return 0;
+}
